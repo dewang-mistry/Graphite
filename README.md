239e089dee2024e6f8fe4347c155384c5992c2ee
unknown <303626@WKSWUMTV1982.hqintl1.com>
2015-04-17 17:39:21 -0400

    Add TinyDB and Markdown support

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ graphite
 ========
 
 Personal knowledge management and learning system.
+
+## Dependencies
+
+	
--- app.py
+++ app.py
@@ -1,36 +1,69 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask.ext.script import Manager
 from flask_bootstrap import Bootstrap
+from tinydb import TinyDB, where
+from slugify import slugify
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = TinyDB('notebooks.json')
+Markdown(app)
 
 @app.route('/', defaults={'notebook': None})
 @app.route('/<notebook>', methods=['GET', 'POST'])
 def index(notebook):
     #return '<h1>Hello World!</h1>'
     if notebook == None:
-    	notebooks = [];
-    	notebooks.append({'name':'Designer', 'url':'designer', 'desc':'This is some notebook description. Maybe the first sentence.'})
-    	notebooks.append({'name':'Developer', 'url':'developer', 'desc':'This is some notebook description. Maybe the first sentence.'})
-    	notebooks.append({'name':'Data Scientist', 'url':'data-science', 'desc':'This is some notebook description. Maybe the first sentence.'})
-
-    	return render_template('index.html', notebooks=notebooks)
+    	notebooks_list = db.all();
+    	return render_template('index.html', notebooks=notebooks_list)
     else:
-		content = """On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."""
 		if request.method == 'POST':
-			#print(request.form['title'])
-			#print(request.form['content'].split('\r\n'))
-			return render_template('notebook.html', title=notebook, content=content)
+			# Check if the notebook already exists
+			selected_notebook = db.search(where('slug') == notebook)
+			notebook_data = {}
+
+			if selected_notebook:
+				notebook_data['title'] = selected_notebook[0].get('title')
+				notebook_data['slug'] = selected_notebook[0].get('slig')
+				notebook_data['desc'] = selected_notebook[0].get('desc')
+				notebook_data['content'] = '\n'.join(selected_notebook[0].get('content'))
+
+				db.update({'content': request.form['content'].split('\r\n'), 'desc':request.form['desc'], 'title':request.form['title']}, where('slug') == notebook)
+			else:
+				notebook_data['title'] = request.form['title']
+				notebook_data['slug'] = slugify(request.form['title'])
+				notebook_data['desc'] = request.form['desc']
+				notebook_data['content'] = request.form['content'].split('\r\n')
+
+				db.insert(notebook_data)
+
+			#return render_template('notebook.html', notebook=notebook_data)
+			return redirect(notebook)
 		else:
+			notebook_data = {}
 			mode = request.args.get('m')
+			selected_notebook = db.search(where('slug') == notebook)
+			
+			if selected_notebook:
+				notebook_data['title'] = selected_notebook[0].get('title')
+				notebook_data['desc'] = selected_notebook[0].get('desc')
+			else:
+				notebook_data['title'] = ''
+				notebook_data['desc'] = ''
+				notebook_data['content'] = ''
 
 			if mode == None:
-				return render_template('notebook.html', title=notebook, content=content)
-			else:
-				return render_template('notebook-edit.html', title=notebook, content=content)
+				if selected_notebook:
+					notebook_data['content'] = '\n'.join(selected_notebook[0].get('content'))
 
+				return render_template('notebook.html', notebook=notebook_data)
+			else:
+				if selected_notebook:
+					notebook_data['content'] = '\r\n'.join(selected_notebook[0].get('content'))
+
+				return render_template('notebook-edit.html', notebook=notebook_data)
 
 @app.route('/notebook/<notebook_name>')
 def notebook(notebook_name):
