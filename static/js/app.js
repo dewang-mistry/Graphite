28d349409876bb0eb6b06be7a333eff1bf12668d
Dewang Mistry <dewang@gmail.com>
2015-04-18 16:39:50 -0400

    Add Eclipse theme for CodeMirror, add support for metadata in notebooks

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,7 @@ $(function() {
 	var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("notebook-editor"), {
 		smartIndent:true,
 		lineNumbers: true,
-		autofocus:true
+		autofocus:true,
+		disableSpellcheck:false
 	});
 });
